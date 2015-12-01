e915033958f068b2213dbafacfafca8aa98d99a8
resteve <resteve@zikzakmedia.com>
2015-12-01 11:14:55 +0100

    Tests

--- tests/test_product_quantity_by_location.py
+++ tests/test_product_quantity_by_location.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
+# This file is part of the product_quantity_by_location module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ProductQuantityByLocationTestCase(unittest.TestCase):
+class ProductQuantityByLocationTestCase(ModuleTestCase):
     'Test Product Quantity By Location module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'product_quantity_by_location')
-
-    def test0005views(self):
-        'Test views'
-        test_view('product_quantity_by_location')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'product_quantity_by_location'
 
 
 def suite():
